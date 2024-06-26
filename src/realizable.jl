e4f54a3d0f05469a4d7bae53db2015318796a0e4
michael d. catchen <mdcatchen@gmail.com>
2024-06-26 11:49:19 -0400

    :zap: realizability speedup

--- src/realizable.jl
+++ src/realizable.jl
@@ -4,30 +4,36 @@ struct NeutrallyForbiddenLinks <: RealizationModel
 end
 struct Equal <: RealizationModel end
 
-function _rate_matrix(localnet, relabd, energy)
+function _rate_matrix(localnet, relabd_mat, energy)
     if !isnothing(localnet)
-        rates = Quantitative(zeros(Float32, size(localnet)))
-        rate_network = SpeciesInteractionNetwork(localnet.nodes, rates)
+        #rates = Quantitative(zeros(Float32, size(localnet)))
+        #rate_network = SpeciesInteractionNetwork(localnet.nodes, rates)
         ra_sum = 0.0
-        ints = interactions(localnet)
-        for int in ints
-            spᵢ, spⱼ, θᵢⱼ = int
-            rᵢ, rⱼ = relabd[spᵢ], relabd[spⱼ]
-            θᵢⱼ = rᵢ * rⱼ
-            rate_network[spᵢ, spⱼ] = θᵢⱼ
-            ra_sum += θᵢⱼ
+        adj = adjacency(localnet)
+        θ = zeros(Float32, size(adj))
+        if sum(adj) > 0
+            for idx in findall(adj)
+                θᵢⱼ = relabd_mat[idx[1], idx[2]]
+                ra_sum += θᵢⱼ
+            end
+            return SpeciesInteractionNetwork(
+                localnet.nodes,
+                Quantitative(energy .* (θ ./ ra_sum))
+            )
         end
-        return energy .* (rate_network ./ ra_sum)
     end
 end
-
 
 function realizable(
     net::N,
     rm::NeutrallyForbiddenLinks
 ) where {N<:Union{Network{<:Feasible},Network{<:Possible}}}
     relabd, energy = rm.relative_abundance, rm.energy
-    _scale = map(x -> _rate_matrix(x, relabd, energy), net)
+
+    relabd_mat = relabd.abundance .* relabd.abundance'
+
+    _scale = map(x -> _rate_matrix(x, relabd_mat, energy), net)
+
     Network{Realizable}(
         net.species,
         _scale,
